73c2a6184865326c580775b69aff26bdca622d44
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2025-08-22 23:17:01 -0300

    adicionando imagem de logo a tela principal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,37 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View
+} from 'react-native';
 import { Button, Text } from 'react-native-paper';
 
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  touchable: {
+    width: '80%',
+    height: '100%'
+  },
+  buttonLogo: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain'
+  }
+});
+
 export default function HomeScreen() {
+  const logoImage = require('../../assets/images/refeicao.png');
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Teste</Text>
-      <Button
-        mode="containerd"
-        style= {{ marginTop: 16}}
-      >Pressione</Button>
+    <View style={styles.view}>
+      <TouchableOpacity style={styles.touchable}>
+        <Image style={styles.buttonLogo} source={logoImage} />
+      </TouchableOpacity>
     </View>
   );
 }
