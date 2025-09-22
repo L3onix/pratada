d8c1353e1e1e801b5d3f84034d1b20d6b2a9399d
Leonardo Facundes l3onix <leonardo.fm00@gmail.com>
2025-09-22 00:36:14 -0300

    removendo paper provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { PaperProvider } from 'react-native-paper';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -19,13 +18,13 @@ export default function RootLayout() {
   }
 
   return (
-    <PaperProvider>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: false }} />
         <Stack.Screen name="meal" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </PaperProvider>
+    </ThemeProvider>
   );
 }
